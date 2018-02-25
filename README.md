3aea2459b4e22003ec2be14d4f22ad3c13544c42
HaoKunT <1151510533@qq.com>
2018-02-26 01:26:28 +0800

    完成关于部分文件存储的功能，完成页面跳转功能

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # A personal Blog
 
 # Dependencs
-- Django==2.0
-- markdown
-- markdownx
-- pygments
+- Django==2.0 : `pip install django`
+- markdown : `pip install markdown`
+- markdownx : `pip install markdownx`
+- pygments : `pip install django-pygments`
 
 # develop plan
 ## 2018.02.25
-1. aboutme part should be save as markdown file, render it in views.
-2. add paginator input tag, so we can jump to any page.
+1. ~~aboutme part should be save as markdown file, render it in views.~~
+2. ~~add paginator input tag, so we can jump to any page.~~
 3. add search function, according category and title.
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage #实现分页功能
 from blog.models import Article
 # from datetime import datetime
 import markdown
+import codecs
 
 # Create your views here.
 
 def home(request,id='1'):
+    # 第一部分用来处理页码跳转
+    if 'jumpto' in request.GET:
+        return HttpResponseRedirect('/blog/home/{}/'.format(request.GET['jumpto']))
+
+
     id=int(id)
     post_list = Article.objects.all()
 
     paginator = Paginator(post_list, 5)
+
 
     try:
         pages = paginator.page(id)
@@ -27,6 +34,8 @@ def home(request,id='1'):
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
             'markdown.extensions.toc',
+            'markdown.extensions.sane_lists',
+            'markdown.extensions.nl2br',
         ])
     return render(request, 'blog/home.html', {'post_list': posts, 'pages': pages})
 
@@ -39,11 +48,19 @@ def Detail(request,id):
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
+        'markdown.extensions.sane_lists',
+        'markdown.extensions.nl2br',
     ])
     return render(request, 'blog/post.html', {'post':post})
 
 def aboutme(request):
-    content = """
-    本人为武汉大学本科生，专业为地理国情监测，喜好计算机技术，对web，GIS，Linux，SQL均有涉猎。
-    """
+    with codecs.open('custom_file/aboutme.md', encoding='utf-8') as f:
+        content = f.read()
+        content = markdown.markdown(content, extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+            'markdown.extensions.sane_lists',
+            'markdown.extensions.nl2br',
+        ])
     return  render(request, 'blog/aboutme.html', {'content' : content})
